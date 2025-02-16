1aa782fdb27e672e86c143f020c30091a6486972
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2025-02-16 17:44:22 +0600

    tailwind added

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>
+      <h1 className="text-4xl font-bold text-center text-gray-900">
         Book Inn
       </h1>
     </div>
